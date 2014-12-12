6402f567db8ee218de75fcd23ee479804afb36ce
Sugawara <sugawaraaiko@sugawaronoimac2.nakasuda>
2014-12-12 17:21:02 +0900

    省略形

--- agent1.groovy
+++ agent1.groovy
@@ -9,29 +9,17 @@ class agent1 extends TAG {
         println msg
 	// ACLからdo-appointmentを受信
         if (msg._p == 'do-appointment') {
-		// test2へappointmentを送信
-		if(msg.with == 'agent2'){
-		    send('agent2', [_p:'appointment', hour:msg.hour, item:msg.item, with:msg.with])
-		// test3へappointmentを送信
-		} else if(msg.with == 'agent3') {
-                    send('agent3', [_p:'appointment', hour:msg.hour, item:msg.item, with:msg.with])
-		}
+	    send(msg.with, [_p:'appointment', hour:msg.hour, item:msg.item, with:msg.with])
 	// 他のagentからappointmentを受信
-        } else if (msg._p == 'appointment' && msg.with == 'agent') {
+        } else if (msg._p == 'appointment' && msg.with == 'agent1') {
             // schedulに追加
             schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
 
-            // test2へ返信
-            if (msg._f == 'agent2'){
-                send('agent2', [_p:'accept'])
-	    } else if (msg._f == 'agent3'){
-		send('agent3', [_p:'accept'])
-	    }
+	    // acceptを送信
+            send(msg._f, [_p:'accept'])
 
 	    // ACLにinformを送信
             send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
-        } else {
-            reply(msg, [_p:'sorry', msg:msg])
-	}
+        } //  else if (msg._p == 'cansel' &&) {
     }
 }
