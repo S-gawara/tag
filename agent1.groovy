59f80b0388d2148eec11852065b1f989e312bd3b
Sugawara <sugawaraaiko@sugawaronoimac2.nakasuda>
2014-12-13 18:36:19 +0900

    途中

--- agent1.groovy
+++ agent1.groovy
@@ -1,5 +1,8 @@
 class agent1 extends TAG {
+    // def schedule = [_p:要求内容, hour:時間, item:会議, with:agent名, _f:依頼主]
     def schedule = []
+    // agent3 < agent2 < agent1
+    def human = [ agent3:0, agent2:1, agent1:2 ]
 
     void setup() {
         server("localhost:8080")
@@ -12,14 +15,46 @@ class agent1 extends TAG {
 	    send(msg.with, [_p:'appointment', hour:msg.hour, item:msg.item, with:msg.with])
 	// 他のagentからappointmentを受信
         } else if (msg._p == 'appointment' && msg.with == 'agent1') {
-            // schedulに追加
-            schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
+		// scheduleが空かどうかを調べる
+		if(schedule == null) {
+                    // scheduleに追加
+                    schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
+		} else {
+		    // hourが重複しているかどうか調べる
+		    def time = schedule.find{it.hour == msg.hour}
+              	    if (time != null) {
+                        // 優先度の比較
+		        println "先客は" + time.with
+//			def visitor = time.with
+			println "agent3の値は" + human.get(time.with)
 
-	    // acceptを送信
+
+//			def visitor1 = human.find{ it.find == visitor }
+			
+
+//			def visitor = schedule.with
+//			println "visitor" + visitor
+
+			// cancelを送信
+//		　　　　send(msg._f, [_p:'cancel'])
+			// ACLに新しい情報を送信
+//		　　　　send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
+		    }
+		    // 重複していなければ実行
+		    else {
+		        // scheduleに追加
+		        schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
+
+	    	        // acceptを送信
+                        send(msg._f, [_p:'accept'])
+
+	                // ACLにinformを送信
+                        send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
+		    }
+		}
+        } else if (msg._p == 'cancel' && msg.with == 'agent1') { //msg.withでいい？
+            // acceptを送信
             send(msg._f, [_p:'accept'])
-
-	    // ACLにinformを送信
-            send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
-        } //  else if (msg._p == 'cansel' &&) {
+	}
     }
 }
