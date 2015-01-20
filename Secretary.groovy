025c4eef3d5058fbff7ad0668d2ad6e05e45cb0a
S-gawara <ramunebin64@gmail.com>
2015-01-20 11:28:24 +0900

    訂正

--- Secretary.groovy
+++ Secretary.groovy
@@ -2,31 +2,26 @@
 public class Secretary_A extends TAG {
     // def schedule = [_p:要求内容, hour:時間, item:会議, with:agent名, _f:依頼主]
     def schedule = []
-    // Secretary_C = Secretary_A < Secretary_B
-    def human = [ Secretary_C:0, Secretary_A:0, Secretary_B:1 ]
+    // Secretary_A = Secretary_C < Secretary_B
+    def human = [ Secretary_A:0, Secretary_B:1, Secretary_C:0 ]
 
     void setup() {
-        server("localhost:8080")
-    }       
+    }
   
     void loop(Map msg) {
         println msg
 	// ACLからdo-appointmentを受信
         if (msg._p == 'do-appointment') {
 	    send(msg.with, [_p:'appointment', hour:msg.hour, item:msg.item, with:msg.with])
-	    // request(msg.with, [_p:'appointment', hour:msg.hour, item:msg.item, with:msg.with])
-
 	// 他のagentからappointmentを受信
         } else if (msg._p == 'appointment' && msg.with == 'Secretary_A') {
+	    // acceptを送信
+	    send(msg._f, [_p:'accept'])
+
 	    // scheduleが空かどうかを調べる
 	    if(schedule == null) {
                 // scheduleに追加
                 schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
-
-	        // acceptを送信
-	 	send(msg._f, [_p:'accept'])
-		// reply(msg, [_p:'accept'])
-
 		// ACLにinformを送信
                 send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])	
 	    } else {
@@ -35,28 +30,17 @@ public class Secretary_A extends TAG {
               	if (time != null) {
 	            // 先客
 	            def visitor1 = human.get(time.with)
-		    println time.with
 		    // 後客
 		    def visitor2 = human.get(msg._f)
-		    println msg._f
 
+		    // 優先度を比較
 		    if (visitor1 < visitor2) {
-			// 登録されているscheduleを削除
-			schedule = schedule.minus time
-
-	                // acceptを送信
-                        send(msg._f, [_p:'accept'])
-			// reply(msg, [_p:'accept'])
-
 			// cancelを送信
 			send(time.with, [_p:'cancel', hour:time.hour, item:time.item, with:time.with])
-
 		        // scheduleに追加
 			schedule.add([hour:msg.hour, item:msg.item, with:msg._f])                    
-
                         // ACLにinformを送信
                         send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
-
 		    } else {
 		        // refusalを送信
 		        send(msg._f, [_p:'refusal'])
@@ -64,11 +48,6 @@ public class Secretary_A extends TAG {
 		} else {
                 	// scheduleに追加
                 	schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
-
-	        	// acceptを送信
-			send(msg._f, [_p:'accept'])
-			// reply(msg, [_p:'accept'])
-
 			// ACLにinformを送信
                 	send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])	
 		}
@@ -79,13 +58,8 @@ public class Secretary_A extends TAG {
         } else if(msg._p == 'cancel' && msg.with == 'Secretary_A') {
             // acceptを送信
             send(msg._f, [_p:'accept'])
-	    // reply(msg, [_p:'accept'])
-
-	    // 登録されているscheduleを削除
-	    schedule = schedule.minus time
 	}
-	// println "[schedule]:" + schedule
     }
 }
 
-TAG.start("localhost:8080",Secretary_A,Secretary_B,Secretary_C)
+TAG.start("localhost:8080", Secretary_A, Secretary_B, Secretary_C)
