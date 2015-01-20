a755362d4912c9864ed09dde07bd5bce2c784291
S-gawara <ramunebin64@gmail.com>
2015-01-20 12:07:28 +0900

    完成

--- Secretary.groovy
+++ Secretary.groovy
@@ -1,8 +1,8 @@
 // Class名とスクリプト名を一緒にはできない
 public class Secretary_A extends TAG {
-    // def schedule = [_p:要求内容, hour:時間, item:会議, with:agent名, _f:依頼主]
+    // def schedule = [_p:要求内容, hour:時間, item:会議, with:依頼相手, _f:依頼主]
     def schedule = []
-    // Secretary_A = Secretary_C < Secretary_B
+    // 重要度(Secretary_A = Secretary_C < Secretary_B)
     def human = [ Secretary_A:0, Secretary_B:1, Secretary_C:0 ]
 
     void setup() {
@@ -10,20 +10,19 @@ public class Secretary_A extends TAG {
   
     void loop(Map msg) {
         println msg
-	// ACLからdo-appointmentを受信
+	// ACLからdo-appointment受信
         if (msg._p == 'do-appointment') {
 	    send(msg.with, [_p:'appointment', hour:msg.hour, item:msg.item, with:msg.with])
-	// 他のagentからappointmentを受信
+	// 他のSecretaryからappointment受信
         } else if (msg._p == 'appointment' && msg.with == 'Secretary_A') {
-	    // acceptを送信
+	    // accept送信
 	    send(msg._f, [_p:'accept'])
-
-	    // scheduleが空かどうかを調べる
+	    // scheduleが空かどうか調べる
 	    if(schedule == null) {
-                // scheduleに追加
+                // schedule追加
                 schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
-		// ACLにinformを送信
-                send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])	
+		// ACLにinform送信
+		send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
 	    } else {
 	        // hourが重複しているかどうか調べる
 		def time = schedule.find{it.hour == msg.hour}
@@ -33,31 +32,31 @@ public class Secretary_A extends TAG {
 		    // 後客
 		    def visitor2 = human.get(msg._f)
 
-		    // 優先度を比較
+		    // 優先度の比較
 		    if (visitor1 < visitor2) {
-			// cancelを送信
+			// cancel送信
 			send(time.with, [_p:'cancel', hour:time.hour, item:time.item, with:time.with])
-		        // scheduleに追加
-			schedule.add([hour:msg.hour, item:msg.item, with:msg._f])                    
-                        // ACLにinformを送信
-                        send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
+		        // schedule追加
+			schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
 		    } else {
 		        // refusalを送信
 		        send(msg._f, [_p:'refusal'])
 		    }
-		} else {
-                	// scheduleに追加
+		} else { // 時間が重複していなければ，比較必要なし
+                	// schedule追加
                 	schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
-			// ACLにinformを送信
-                	send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])	
+			// ACLにinform送信
+			send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
 		}
 	    }
 	} else if (msg._p == 'accept' && msg.with == 'Secretary_A') {
-	    // scheduleに追加
+	    // schedule追加
 	    schedule.add([hour:msg.hour, item:msg.item, with:msg._f])
         } else if(msg._p == 'cancel' && msg.with == 'Secretary_A') {
-            // acceptを送信
+            // accept送信
             send(msg._f, [_p:'accept'])
+	    // ACLにinform送信
+	    send('user', [_p:'inform', hour:msg.hour, item:msg.item, with:msg._f])
 	}
     }
 }
